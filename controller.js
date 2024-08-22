461255ff1e965c5d98771bcac538d71fc1eabab2
Nayini Bhavani <bhavanindrnf@gmail.com>
2024-08-22 11:01:49 +0530

    mongodb replica update

--- controller.js
+++ controller.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose')
 const dataRouter = require('./routes/requireddata')
 
 
-const url = 'mongodb://127.0.0.1:27017/Student'
+//const url = 'mongodb://127.0.0.1:27017/Student'
+const url='mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/Student?replicaSet=m101';
 const app = express()
 mongoose.connect(url)
 const con = mongoose.connection
