11f136d029231318e2d989545d4107b8e2c0562a
Nayini Bhavani <bhavanindrnf@gmail.com>
2024-08-29 12:05:22 +0530

    mongodb atlass(aws)

--- controller.js
+++ controller.js
@@ -5,7 +5,8 @@ const dataRouter = require('./routes/requireddata')
 
 
 //const url = 'mongodb://127.0.0.1:27017/Student'
-const url='mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/Student?replicaSet=m101';
+//const url='mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/Student?replicaSet=m101';
+const url='mongodb+srv://nayinivennela12:<password>@merncrud.brfel.mongodb.net/?retryWrites=true&w=majority&appName=merncrud';
 const app = express()
 mongoose.connect(url)
 const con = mongoose.connection
